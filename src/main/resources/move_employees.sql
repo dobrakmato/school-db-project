f850161e02ee1d1d5affb89f9b5b022d680035d1
Matej <dobrakmato@gmail.com>
2019-05-08 13:00:00 +0200

    fix move employees operation

--- src/main/resources/move_employees.sql
+++ src/main/resources/move_employees.sql
@@ -17,13 +17,21 @@ with cte as (
      high_empl as (select *, row_number() over (order by id) as num
                    from high_case
                             join assigned_employees on high_case.case_id = assigned_employees.case_id),
-     -- employees to move
+     -- employees of low populated case
+     low_empl as (select *
+                  from low_case
+                           join assigned_employees on low_case.case_id = assigned_employees.case_id),
+     -- employees to move (may violate unique constr)
      empl_to_move as (select *
                       from high_empl,
                            high_case
-                      where num < cast(random() * 0.5 * high_case.cnt as integer))
+                      where num < cast(random() * 0.5 * high_case.cnt as integer)),
+     -- employees to move (safe)
+     empl_to_move_safe as (select *
+                           from empl_to_move
+                           where empl_to_move.employee_id not in (select low_empl.employee_id from low_empl))
 update assigned_employees
 set case_id = low_case.case_id
-from empl_to_move,
+from empl_to_move_safe,
      low_case
-where empl_to_move.id = assigned_employees.id
+where empl_to_move_safe.id = assigned_employees.id
